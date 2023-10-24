a699aa6365584a164319767e545e41468f0fca87
Julia <avokdorog@mail.ru>
2023-10-24 14:54:35 +0300

    first commit

--- src/test/java/ru/netology/CardOrderingTest.java
+++ src/test/java/ru/netology/CardOrderingTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrderingTest {
